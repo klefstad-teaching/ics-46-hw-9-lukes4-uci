2fa0a5d6916e8e31ecc40dc11841072c206b5ac0
lukes4 <lukes4@uci.edu>
2025-03-14 07:05:26 +0000

    hopefully this all just works

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,1 +1,56 @@
 #include "djikstras.h"
+#include <iostream>
+
+class MinHeapComparator() {
+    public: 
+        int operator() (const std::pair<int, int> a, const std::pair<int, int> b) {
+            return a.second < b.second; // puts the lowest weight first
+        }
+};
+
+std::vector<int> dijkstra_shortest_path(const Graph& G, int source, std::vector<int>& previous) {
+    std::vector<int> distances(numVertices, INF);
+    std::vector<bool> visited(numVertices, false);
+    distances[source] = 0;
+    previous[source] = UNDEFINED; 
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, MinHeapComparator> minHeap;
+    minHeap.push({source, 0});
+
+    while(!minHeap.empty()) {
+        int u = minHeap.top().first();
+        minHeap.pop();
+        if(visited[u]) continue;
+        visited[u] = true;
+        for(Edge e : G[u]) {
+            int v = e.dst;
+            int weight = e.weight;
+            if(!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push(v, distances[v]);
+            }
+        }
+    }
+    return distances;
+}
+
+std::vector<int> extract_shortest_path(const std::vector<int>& distances, const std::vector<int>& previous, int destination) {
+    std::stack<int> vertex_stack;
+    for(int u = previous[destination]; u != 0; u = previous[u]) {
+        vertex_stack.push(u);
+    }
+    vertex_stack.push(0);
+    std::vector<int> ret;
+    while(!vertex_stack.empty) {
+        u = vertex_stack.top();
+        ret.push_back(u);
+        vertex_stack.pop();
+    }
+    return ret;
+}
+void print_path(const std::vector<int>& v, int total) {
+    for(auto i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl << "Total cost is " << total << std::endl;
+}
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,5 +1,7 @@
 #include "ladder.h"
 #include <cmath>
+#include <iostream>
+#include <fstream>
 
 void error(string word1, string word2, string msg);
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
@@ -30,7 +32,7 @@ std::vector<std::string> generate_word_ladder(const std::string& begin_word, con
         ladder_queue.pop();
         std::string last_word = ladder[ladder.size() - 1];
         for(std::string word : word_list) {
-            if(!is_adjacent(last_word, word) && visited.find(word) == visited.end()) continue;
+            if(!(is_adjacent(last_word, word) && visited.find(word) == visited.end())) continue;
             visited.insert(word);
             vector<std::string> new_ladder = ladder;
             new_ladder.push_back(word);
@@ -42,6 +44,37 @@ std::vector<std::string> generate_word_ladder(const std::string& begin_word, con
     std::vector<std::string> empty;
     return empty;
 }
-void load_words(set<string> & word_list, const string& file_name);
-void print_word_ladder(const vector<string>& ladder);
-void verify_word_ladder();
+void load_words(set<std::string> & word_list, const std::string& file_name) {
+    std::ifstream in(file_name);
+    for(std::string w; w >> in;) {
+        word_list.insert(w);
+    } 
+}
+void print_word_ladder(const vector<std::string>& ladder) {
+    for(std::string w : ladder) {
+        std::cout << w << " ";
+    }
+}
+
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+
+void verify_word_ladder() {
+
+    set<std::string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
+}
+
