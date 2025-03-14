7926b73b2e37595485f85de2ff9503e402aa1eea
lukes4 <lukes4@uci.edu>
2025-03-14 08:04:37 +0000

    well at least dijkstra's works

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -1,5 +1,22 @@
 #include "ladder.h"
+#include <iostream>
 
 int main() {
-    verify_word_ladder();
+    set<std::string> word_list;
+
+    load_words(word_list, "words.txt");
+    std::vector<std::string> cat_dog = generate_word_ladder("cat", "dog", word_list);
+    print_word_ladder(cat_dog);
+
+    // my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    // my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    // my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    // my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    // my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    // my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
